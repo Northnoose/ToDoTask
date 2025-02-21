e3aff03bc35b9a5ea8760e31ffc66465c54ed956
Steffen <Steffen.Nordnes@hotmail.com>
2025-02-21 20:50:54 +0100

    Små pynt

--- app/src/main/java/com/example/todotask/uiscreen/CheckListItem.kt
+++ app/src/main/java/com/example/todotask/uiscreen/CheckListItem.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -32,7 +33,6 @@ enum class DialogType {
 
 @Composable
 fun CheckListItem(
-    index: Int,
     checkList: MyCheckList,
     onDelete: () -> Unit,
     onToggle: (Int) -> Unit,
@@ -45,7 +45,7 @@ fun CheckListItem(
     var newName by remember { mutableStateOf(checkList.name) }
     var isExpanded by remember { mutableStateOf(false) }
     var editedItems by remember { mutableStateOf(checkList.items.map {it.text}) }
-    var itemToDelete by remember { mutableStateOf(-1) }
+    var itemToDelete by remember { mutableIntStateOf(-1) }
     var dialogType by remember { mutableStateOf<DialogType?>(null) }
 
     ElevatedCard(
--- app/src/main/java/com/example/todotask/uiscreen/CheckListScreen.kt
+++ app/src/main/java/com/example/todotask/uiscreen/CheckListScreen.kt
@@ -1,18 +1,25 @@
 package com.example.todotask.uiscreen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.todotask.viewmodel.CheckListViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -22,13 +29,27 @@ fun CheckListScreen(
     modifier: Modifier = Modifier,
     viewModel: CheckListViewModel = viewModel()) {
     Column(modifier = modifier){
-        TopAppBar(title = { Text("ToDo Huskelister") })
-
         var showIconPicker by remember { mutableStateOf(false) }
+        TopAppBar(
+            title = {
+                Row(modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically){
+                    Text(
+                        "ToDo Huskelister",
+                        style = MaterialTheme.typography.headlineMedium, // 🔹 Større og fet skrift
+                        modifier = Modifier.padding(start = 16.dp).weight(1f)
+                    )
+                    Button(onClick = { showIconPicker = true }) {
+                        Text("Ny Liste")
+                    }
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            )
 
-        Button(onClick = { showIconPicker = true }){
-            Text("Ny Liste")
-        }
 
         if(showIconPicker) {
             IconPickerDialog(
@@ -43,11 +64,10 @@ fun CheckListScreen(
         LazyColumn {
             itemsIndexed(viewModel.checkLists.value) { index, checkList ->
                 CheckListItem(
-                    index = index,
                     checkList = checkList,
                     onDelete = { viewModel.deleteList(index) },
                     onToggle = { taskIndex -> viewModel.toggleTask(index, taskIndex)},
-                    onRename = { newName -> viewModel.renameList(index, newName.toString()) },
+                    onRename = { newName -> viewModel.renameList(index, newName) },
                     onEditItem = {indexItem, newText -> viewModel.renameItemList(index, indexItem, newText)},
                     onDeleteItem = { indexItem -> viewModel.deleteListItem(index, indexItem) },
                     onAddItem = { newText -> viewModel.addListItem(index, newText)}
